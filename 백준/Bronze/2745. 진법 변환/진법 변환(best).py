ebcff9bca41cda349c05bc4589e10966fa3ea7d2
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2024-03-21 19:25:43 +0900

    Update and rename 진법 변환.py to 진법 변환(best).py

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환(best).py
@@ -0,0 +1,4 @@
+n, b = input().split()
+print(int(n, int(b)))
+
+# int(str, int) 2<=int<=36 str을 int진법으로 변환해줌 
--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++
@@ -1,4 +0,0 @@
-n, b = input().split()
-print(int(n, int(b)))
-
-# int(str, int) 2<=int<=36 str을 int진법으로 변환해줌 
